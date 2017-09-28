06150faeaa52f586fec2507b687e2a212de65f34
joao lucas <joao_lukas97@hotmail.com>
2017-09-28 19:59:57 -0300

    api, front com bootstrap

--- JDI/app.js
+++ JDI/app.js
@@ -3,14 +3,19 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/login", function(req, res){
+app.get("/login", function (req, res) {
     res.render("login/login");
 });
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("index");
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Servidor rodando");
 });
+
+app.use('/', express.static(__dirname + '/www'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); 
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
