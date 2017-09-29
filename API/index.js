f448bba03029165091a2639998270b978b272fda
joao lucas <joao_lukas97@hotmail.com>
2017-09-28 22:30:58 -0300

    firebase admin config

--- API/index.js
+++ API/index.js
@@ -1,5 +1,18 @@
 
 var app = require('./config/custom-express')();
+var admin = require("firebase-admin");
+var serviceAccount = require("../justdoit-2017-firebase-adminsdk-5x949-a9e6948afd.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://justdoit-2017.firebaseio.com/"
+});
+
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
 
 app.listen(3000, function(){
     console.log('Servidor rodando na porta 3000');
